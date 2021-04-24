cc4854327120d23057c968c75f1784e3389035ae
Kamal (Riwogo) <pmer93@gmail.com>
2021-04-24 20:42:53 +0430

    add forall_exists_challenge exercise

--- lf/Tactics.v
+++ lf/Tactics.v
@@ -1294,42 +1294,57 @@ Qed.
     [existsb'] and [existsb] have the same behavior.
 *)
 
-Fixpoint forallb {X : Type} (test : X -> bool) (l : list X) : bool
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Definition forallb {X : Type} (test : X -> bool) (l : list X) : bool :=
+  fold andb (map test l) true.
 
 Example test_forallb_1 : forallb oddb [1;3;5;7;9] = true.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_forallb_2 : forallb negb [false;false] = true.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_forallb_3 : forallb evenb [0;2;4;5] = false.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_forallb_4 : forallb (eqb 5) [] = true.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
-Fixpoint existsb {X : Type} (test : X -> bool) (l : list X) : bool
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Definition existsb {X : Type} (test : X -> bool) (l : list X) : bool :=
+  fold orb (map test l) false.
 
 Example test_existsb_1 : existsb (eqb 5) [0;2;3;6] = false.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_existsb_2 : existsb (andb true) [true;true;false] = true.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_existsb_3 : existsb oddb [1;0;0;0;0;3] = true.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
 Example test_existsb_4 : existsb evenb [] = false.
-Proof. (* FILL IN HERE *) Admitted.
+Proof. reflexivity. Qed.
 
-Definition existsb' {X : Type} (test : X -> bool) (l : list X) : bool
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Definition existsb' {X : Type} (test : X -> bool) (l : list X) : bool :=
+  negb (forallb (fun x => negb (test x)) l).
 
 Theorem existsb_existsb' : forall (X : Type) (test : X -> bool) (l : list X),
   existsb test l = existsb' test l.
-Proof. (* FILL IN HERE *) Admitted.
+Proof.
+  intros X test l.
+  induction l as [| n l' IHl'].
+  - simpl. unfold existsb'. reflexivity.
+  - simpl. unfold existsb'.
+    + destruct (test n) eqn:E.
+      ++ unfold existsb. unfold forallb.
+        simpl. rewrite -> E.
+        simpl. reflexivity.
+      ++ unfold existsb. unfold forallb.
+         simpl. rewrite -> E. simpl.
+         unfold existsb in IHl'.
+         unfold existsb' in IHl'.
+         unfold forallb in IHl'.
+         apply IHl'.
+Qed.
 
 (** [] *)
 
